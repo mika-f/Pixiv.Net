f5dcc512622d2d4f7d9b5523109352a4550d0a17
Mikazuki <mikazuki_fuyuno@outlook.com>
2017-03-20 15:19:27 +0900

    :bug: Fix for authenticated user

--- Source/Sagitta/Models/ProfileImageUrls.cs
+++ Source/Sagitta/Models/ProfileImageUrls.cs
@@ -6,5 +6,21 @@ namespace Sagitta.Models
     {
         [JsonProperty("medium")]
         public string Medium { get; set; }
+
+        /// <summary>
+        ///     16x16
+        /// </summary>
+        [JsonProperty("px_16x16")]
+        public string Small { get; set; }
+
+        [JsonProperty("px_50x50")]
+        private string MediumInternal
+        {
+            get { return Medium; }
+            set { Medium = value; }
+        }
+
+        [JsonProperty("px_170x170")]
+        public string Large { get; set; }
     }
 }
