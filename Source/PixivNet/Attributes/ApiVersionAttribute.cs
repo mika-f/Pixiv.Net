4424eb2ac947ed1cb1d5b941f9de9843732bce3f
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-10-05 18:16:55 +0900

    chore: changed root directory to PixivNet

---
+++ Source/PixivNet/Attributes/ApiVersionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pixiv.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property)]
+    internal class ApiVersionAttribute : Attribute
+    {
+        public string Version { get; }
+
+        public ApiVersionAttribute(string version)
+        {
+            Version = version;
+        }
+    }
+}
