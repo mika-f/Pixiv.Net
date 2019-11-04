a384afc7482f8690dea4fd37bc70c39ebee7d814
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-04 18:43:44 +0900

    feat: add base api client

--- Source/PixivNet/Clients/ApiClient.cs
+++ Source/PixivNet/Clients/ApiClient.cs
@@ -1,18 +1,31 @@
-﻿namespace Pixiv.Clients
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+using Pixiv.Extensions;
+
+namespace Pixiv.Clients
 {
-    /// <summary>
-    ///     API Client ベースクラス
-    /// </summary>
     public class ApiClient
     {
-        /// <summary>
-        ///     <see cref="PixivClient" /> インスタンス
-        /// </summary>
-        protected PixivClient PixivClient { get; }
+        private readonly string _base;
+        protected PixivClient Client { get; }
 
-        protected ApiClient(PixivClient pixivClient)
+        public ApiClient(PixivClient client, string @base)
         {
-            PixivClient = pixivClient;
+            Client = client;
+            _base = @base;
+        }
+
+        public async Task<T> GetAsync<T>(string endpoint = "", List<KeyValuePair<string, object>>? parameters = null)
+        {
+            return await Client.GetAsync<T>(_base + endpoint, parameters).Stay();
+        }
+
+        public async Task<JObject> GetAsync(string endpoint = "", List<KeyValuePair<string, object>>? parameters = null)
+        {
+            return await Client.GetAsync(_base + endpoint, parameters).Stay();
         }
     }
 }
