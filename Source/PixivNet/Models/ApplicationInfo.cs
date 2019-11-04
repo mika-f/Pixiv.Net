6f293f6bfcade99bcc53b33d8c2678c07c2e893c
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-04 18:37:06 +0900

    feat: order and add attributes for developing

--- Source/PixivNet/Models/ApplicationInfo.cs
+++ Source/PixivNet/Models/ApplicationInfo.cs
@@ -1,64 +1,54 @@
 ﻿using Newtonsoft.Json;
 
+using Pixiv.Attributes;
+
 namespace Pixiv.Models
 {
-    /// <summary>
-    ///     アプリケーション情報
-    /// </summary>
     public class ApplicationInfo : ApiResponse
     {
-        /// <summary>
-        ///     リリースされている最新バージョンの番号
-        /// </summary>
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
         [JsonProperty("latest_version")]
         public string LatestVersion { get; set; }
 
-        /// <summary>
-        ///     現在使用しているクライアントに対してアップデートが必要か否か
-        /// </summary>
-        [JsonProperty("update_required")]
-        public bool IsUpdateRequired { get; set; }
-
-        /// <summary>
-        ///     アップデートが利用可能か
-        /// </summary>
-        [JsonProperty("update_available")]
-        public bool IsUpdateAvailable { get; set; }
-
-        /// <summary>
-        ///     更新用メッセージ
-        /// </summary>
-        [JsonProperty("update_message")]
-        public string UpdateMessage { get; set; }
-
-        /// <summary>
-        ///     通知があるか否か
-        /// </summary>
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
         [JsonProperty("notice_exists")]
         public bool IsNoticeExists { get; set; }
 
-        /// <summary>
-        ///     ストア URL
-        /// </summary>
-        [JsonProperty("store_url")]
-        public string StoreUrl { get; set; }
-
-        /// <summary>
-        ///     通知 ID
-        /// </summary>
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
         [JsonProperty("notice_id")]
         public string NoticeId { get; set; }
 
-        /// <summary>
-        ///     通知が重要かどうか
-        /// </summary>
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
         [JsonProperty("notice_important")]
         public bool IsNoticeImportant { get; set; }
 
-        /// <summary>
-        ///     通知メッセージ
-        /// </summary>
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
         [JsonProperty("notice_message")]
         public string NoticeMessage { get; set; }
+
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
+        [JsonProperty("store_url")]
+        public string StoreUrl { get; set; }
+
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
+        [JsonProperty("update_available")]
+        public bool IsUpdateAvailable { get; set; }
+
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
+        [JsonProperty("update_required")]
+        public bool IsUpdateRequired { get; set; }
+
+        [ApiVersion]
+        [MarkedAs("7.7.7")]
+        [JsonProperty("update_message")]
+        public string UpdateMessage { get; set; }
     }
 }
