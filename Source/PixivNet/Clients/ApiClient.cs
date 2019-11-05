21be2fdfe8749edc078b26a1114dedd2c6ec0f72
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-05 23:54:53 +0900

    fix: pixiv api has multiple domains

--- Source/PixivNet/Clients/ApiClient.cs
+++ Source/PixivNet/Clients/ApiClient.cs
@@ -10,22 +10,24 @@ namespace Pixiv.Clients
     public class ApiClient
     {
         private readonly string _base;
+        private readonly string _host;
         protected PixivClient Client { get; }
 
-        public ApiClient(PixivClient client, string @base)
+        public ApiClient(PixivClient client, string @base, string host = "app-api.pixiv.net")
         {
             Client = client;
             _base = @base;
+            _host = host;
         }
 
         public async Task<T> GetAsync<T>(string endpoint = "", List<KeyValuePair<string, object>>? parameters = null)
         {
-            return await Client.GetAsync<T>(_base + endpoint, parameters).Stay();
+            return await Client.GetAsync<T>($"https://{_host}{_base}{endpoint}", parameters).Stay();
         }
 
         public async Task<JObject> GetAsync(string endpoint = "", List<KeyValuePair<string, object>>? parameters = null)
         {
-            return await Client.GetAsync(_base + endpoint, parameters).Stay();
+            return await Client.GetAsync($"https://{_host}{_base}{endpoint}", parameters).Stay();
         }
     }
 }
