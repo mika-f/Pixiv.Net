a4164d1710b15e7577dceda47ee78b7e168a2a2d
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-06 01:04:46 +0900

    feat: add shims package

---
+++ Source/PixivNet.Shims/PixivClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+using Pixiv;
+
+namespace PixivNet.Shims
+{
+    public static class PixivClientFactory
+    {
+        public static PixivClient CreateWith(string version, HttpMessageHandler? handler = null)
+        {
+            switch (version)
+            {
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
