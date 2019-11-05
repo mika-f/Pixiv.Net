28aa62df20fe6a8616f9f8332902fe8d0a3123d4
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-06 00:40:45 +0900

    fix: invalid url and object path

--- Source/PixivNet/Clients/V1/ApplicationInfoClient.cs
+++ Source/PixivNet/Clients/V1/ApplicationInfoClient.cs
@@ -8,7 +8,7 @@ namespace Pixiv.Clients.V1
 {
     public class ApplicationInfoClient : ApiClient
     {
-        public ApplicationInfoClient(PixivClient client) : base(client, "/v1/application-infoV1") { }
+        public ApplicationInfoClient(PixivClient client) : base(client, "/v1/application-info") { }
 
         // ReSharper disable once UnusedMember.Global
         // ReSharper disable once InconsistentNaming
@@ -16,8 +16,8 @@ namespace Pixiv.Clients.V1
         [MarkedAs("7.7.7")]
         public async Task<ApplicationInfo> IOSAsync()
         {
-            var obj = await GetAsync().Stay();
-            return obj["application-info"].ToObject<ApplicationInfo>();
+            var obj = await GetAsync("/ios").Stay();
+            return obj["application_info"].ToObject<ApplicationInfo>();
         }
     }
 }
