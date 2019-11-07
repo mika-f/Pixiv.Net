7514875f0222253a0b3246f2ceec78788e386d78
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-08 00:14:31 +0900

    chore: use Uri insteaf of string

--- Source/PixivNet/Models/ApplicationInfo.cs
+++ Source/PixivNet/Models/ApplicationInfo.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+
+using Newtonsoft.Json;
 
 using Pixiv.Attributes;
 
@@ -6,6 +8,8 @@ namespace Pixiv.Models
 {
     public class ApplicationInfo : ApiResponse
     {
+#pragma warning disable CS8618 // Null 非許容フィールドは初期化されていません。null 許容として宣言することを検討してください。
+
         [ApiVersion]
         [MarkedAs("7.7.7")]
         [JsonProperty("latest_version")]
@@ -34,7 +38,7 @@ namespace Pixiv.Models
         [ApiVersion]
         [MarkedAs("7.7.7")]
         [JsonProperty("store_url")]
-        public string StoreUrl { get; set; }
+        public Uri StoreUrl { get; set; }
 
         [ApiVersion]
         [MarkedAs("7.7.7")]
@@ -50,5 +54,7 @@ namespace Pixiv.Models
         [MarkedAs("7.7.7")]
         [JsonProperty("update_message")]
         public string UpdateMessage { get; set; }
+
+#pragma warning restore CS8618 // Null 非許容フィールドは初期化されていません。null 許容として宣言することを検討してください。
     }
 }
