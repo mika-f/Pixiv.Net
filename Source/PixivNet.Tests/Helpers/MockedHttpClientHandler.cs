cc55d1afd16d1c2b7189f6f99cb83febcf819b6d
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-08 12:01:14 +0900

    feat: remove secure parameters/responses from request/response

--- Source/PixivNet.Tests/Helpers/MockedHttpClientHandler.cs
+++ Source/PixivNet.Tests/Helpers/MockedHttpClientHandler.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 using Pixiv.Tests.Models;
 
@@ -16,15 +18,26 @@ namespace Pixiv.Tests.Helpers
         private readonly List<Cassette> _cassettes;
         private readonly string _fixturePath;
         private readonly List<string> _privateHeaders;
+        private readonly List<string> _privateParameters;
+        private readonly List<string> _privateResponses;
 
-        public MockedHttpClientHandler(string fixturePath, List<string>? privateHeaders = null)
+        public MockedHttpClientHandler(string fixturePath, List<string>? privateHeaders = null, List<string>? privateParameters = null, List<string>? privateResponses = null)
         {
             _fixturePath = fixturePath;
             _privateHeaders = privateHeaders ?? new List<string>();
+            _privateParameters = privateParameters ?? new List<string>();
+            _privateResponses = privateResponses ?? new List<string>();
             _cassettes = new List<Cassette>();
 
-            // default private headers
+            // default private keys
             _privateHeaders.Add("Authorization");
+            _privateParameters.Add("username");
+            _privateParameters.Add("password");
+            _privateParameters.Add("client_id");
+            _privateParameters.Add("client_secret");
+            _privateResponses.Add("access_token");
+            _privateResponses.Add("refresh_token");
+            _privateResponses.Add("mail_address");
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -46,13 +59,13 @@ namespace Pixiv.Tests.Helpers
                 {
                     Uri = request.RequestUri,
                     Headers = request.Headers.ToDictionary(pair => pair.Key, pair => _privateHeaders.Contains(pair.Key) ? new List<string> { "**REDACTED**" } : pair.Value),
-                    Body = request.Content == null ? "" : await request.Content.ReadAsStringAsync()
+                    Body = request.Content == null ? "" : RedactPrivateParameters(await request.Content.ReadAsStringAsync())
                 },
                 Response = new CassetteResponse
                 {
                     StatusCode = response.StatusCode,
                     Headers = response.Headers.ToDictionary(pair => pair.Key, pair => _privateHeaders.Contains(pair.Key) ? new List<string> { "**REDACTED**" } : pair.Value),
-                    Body = await response.Content.ReadAsStringAsync()
+                    Body = RedactPrivateResponses(await response.Content.ReadAsStringAsync())
                 }
             };
             _cassettes.Add(cassette);
@@ -93,7 +106,7 @@ namespace Pixiv.Tests.Helpers
                 return;
 
             using var sr = new StreamReader(path);
-            _cassettes.AddRange(JsonSerializer.Deserialize<List<Cassette>>(sr.ReadToEnd()));
+            _cassettes.AddRange(JsonConvert.DeserializeObject<List<Cassette>>(sr.ReadToEnd()));
         }
 
         private void FlushFixturesToPath(string path)
@@ -102,7 +115,34 @@ namespace Pixiv.Tests.Helpers
                 Directory.CreateDirectory(Path.GetDirectoryName(path));
 
             using var sw = new StreamWriter(path);
-            sw.WriteLine(JsonSerializer.Serialize(_cassettes));
+            sw.WriteLine(JsonConvert.SerializeObject(_cassettes));
+        }
+
+        private string RedactPrivateParameters(string form)
+        {
+            var obj = form.Split("&").Select(w => (w.Split("=")[0], w.Split("=")[1]));
+            var parameters = obj.Select(tuple => _privateParameters.Contains(tuple.Item1) ? $"{tuple.Item1}=**REDACTED**" : $"{tuple.Item1}={tuple.Item2}").ToList();
+
+            return string.Join("&", parameters);
+        }
+
+        private string RedactPrivateResponses(string json)
+        {
+            JObject RecursiveRedact(JObject o)
+            {
+                foreach (var (key, value) in o)
+                {
+                    if (_privateResponses.Contains(key))
+                        o[key] = "**REDACTED**";
+                    if (value is JObject jsonObj)
+                        o[key] = RecursiveRedact(jsonObj);
+                }
+
+                return o;
+            }
+
+            var obj = JObject.Parse(json);
+            return RecursiveRedact(obj).ToString();
         }
     }
 }
