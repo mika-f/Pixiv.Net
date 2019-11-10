b2372280a326df1ac2a410bbf62147cfa9f8c1ea
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-10 17:08:12 +0900

    refactor: add pragma

--- Source/PixivNet/Models/CommentCollection.cs
+++ Source/PixivNet/Models/CommentCollection.cs
@@ -9,6 +9,8 @@ namespace Pixiv.Models
 {
     public class CommentCollection : ApiResponse
     {
+#pragma warning disable CS8618 // Null 非許容フィールドは初期化されていません。null 許容として宣言することを検討してください。
+
         [ApiVersion]
         [MarkedAs("7.7.7")]
         [JsonProperty("comments")]
@@ -18,5 +20,7 @@ namespace Pixiv.Models
         [MarkedAs("7.7.7")]
         [JsonProperty("next_url")]
         public Uri? NextUrl { get; set; }
+
+#pragma warning restore CS8618 // Null 非許容フィールドは初期化されていません。null 許容として宣言することを検討してください。
     }
 }
