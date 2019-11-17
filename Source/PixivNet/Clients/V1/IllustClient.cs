8dcee0f50e801cc16a113d04355629c23b210e06
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-17 16:58:11 +0900

    feat: support illust bookmark apis

---
+++ Source/PixivNet/Clients/V1/IllustClient.cs
@@ -0,0 +1,14 @@
+﻿using Pixiv.Clients.V1.Illust;
+
+namespace Pixiv.Clients.V1
+{
+    public class IllustClient : ApiClient
+    {
+        public BookmarkClient Bookmark { get; }
+
+        internal IllustClient(PixivClient client) : base(client, "/v1/illust")
+        {
+            Bookmark = new BookmarkClient(client);
+        }
+    }
+}
