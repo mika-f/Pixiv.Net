e3d0ffefe371260aefe665e2a9c2f235c03bcb56
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-11-20 19:11:27 +0900

    feat: support novel bookmark api

---
+++ Source/PixivNet/Clients/V1/NovelClient.cs
@@ -0,0 +1,14 @@
+﻿using Pixiv.Clients.V1.Novel;
+
+namespace Pixiv.Clients.V1
+{
+    public class NovelClient : ApiClient
+    {
+        public BookmarkClient Bookmark { get; }
+
+        internal NovelClient(PixivClient client) : base(client, "/v1/novel")
+        {
+            Bookmark = new BookmarkClient(client);
+        }
+    }
+}
